4510b663a016c6db8d007139d76f50152267671f
leonchemnitz <leonchemnitz@gmail.com>
2019-11-30 10:08:31 +0100

    created Branch

---
+++ src/main/java/de/bp2019/zentraldatei/view/ManageModuleSchemesView.java
@@ -0,0 +1,14 @@
+package de.bp2019.zentraldatei.view;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("moduleSchemes")
+public class ManageModuleSchemesView extends VerticalLayout {
+
+    private static final long serialVersionUID = 1L;
+
+    public ManageModuleSchemesView() {
+    }
+
+}
