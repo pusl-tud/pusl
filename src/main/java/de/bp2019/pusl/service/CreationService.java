465358e9d0dfe640dcfdf9106309404b75deb6f4
leonchemnitz <leonchemnitz@gmail.com>
2020-02-06 20:49:02 +0100

    implememted user management

---
+++ src/main/java/de/bp2019/pusl/service/CreationService.java
@@ -0,0 +1,38 @@
+package de.bp2019.pusl.service;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import de.bp2019.pusl.enums.UserType;
+import de.bp2019.pusl.model.User;
+import de.bp2019.pusl.repository.UserRepository;
+
+/**
+ * Ths Creation Service is used to add the first admin to the Database, when the
+ * Application is started for the first time. The first user has emailAddres
+ * admin and password admin and is only added if no other users exist in the
+ * database
+ * 
+ * @author Leon Chemnitz
+ */
+@Service
+public class CreationService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+        if (userRepository.findAll().size() == 0) {
+            User admin = new User();
+            admin.setEmailAddress("admin");
+            admin.setPassword(passwordEncoder.encode("admin"));
+            admin.setType(UserType.SUPERADMIN);
+            userRepository.save(admin);
+        }
+    }
+}
